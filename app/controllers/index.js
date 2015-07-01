4f569b2a1b97ee12b2b2ebae5847d6a8772a8175
Geo Milano <george_limkt@hotmail.com>
2015-07-01 13:02:41 +0800

    minor fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -386,7 +386,7 @@ Ti.App.addEventListener('app:loadCategory', function(e){
 	syncCategory();
 });
 
-$.indexView.filter_link.addEventListener('click', function(e){
+$.indexView.home.addEventListener('click', function(e){
 	var dialog = Ti.UI.createOptionDialog({
 	  cancel: 2,
 	  options: ['Recent', 'Popular', 'Cancel'],
